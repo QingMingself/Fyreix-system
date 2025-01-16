1da52c7ddee11e211b19c427916ad1ea8bd23bba
QING MING <148698302+QingMingself@users.noreply.github.com>
2025-01-16 16:24:17 +0800

    Update live2d.ts

--- src/constants/live2d.ts
+++ src/constants/live2d.ts
@@ -1,3 +1,3 @@
 export const roleToLive2dMapper: unknown = {
-  小饭: { path: "/src/MissF/missf.model3.json", scale1: 0.42, scale2: 0.42, x1: 550, x2: -100, y1: 50, y2: 20 },
+  小饭: { path: "https://static.amadeus-web.top/live2dmodels/steinsGateKurisuNew/红莉栖.model3.json", scale1: 0.42, scale2: 0.42, x1: 550, x2: -100, y1: 50, y2: 20 },
 }
