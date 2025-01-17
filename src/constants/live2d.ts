8896c1cbbff540f695ea6fa9601a1e3777ec39dc
QING MING <148698302+QingMingself@users.noreply.github.com>
2025-01-17 16:40:58 +0800

    Update live2d.ts

--- src/constants/live2d.ts
+++ src/constants/live2d.ts
@@ -1,3 +1,3 @@
 export const roleToLive2dMapper: unknown = {
-  牧濑红莉栖: { path: "/src/MissF/missf.model3.json", scale1: 2.0, scale2: 2.0, x1: 0, x2: 0, y1: 0, y2: 0 },
+  小饭: { path: "/src/MissF/missf.model3.json", scale1: 2.0, scale2: 2.0, x1: 0, x2: 0, y1: 0, y2: 0 },
 }
